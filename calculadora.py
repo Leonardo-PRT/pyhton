6b98970c5dca0628a26f3704b4e8f033205fa388
Leonardo Porto <64111747+Leonardo-PRT@users.noreply.github.com>
2020-10-30 16:12:57 -0300

    Adicionando comentários no código

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-
+''' Pegando input do usuário '''
 num1 = float(input('Digite um número:'))
 op = str(input('Digite a operação: '))
 num2 = float(input('Digite outro número:'))
@@ -6,6 +6,7 @@ res = 0
 
 cont = True
 
+''' Função para calcular de acordo com input do usuário '''
 def calcular(num1, num2):
 
     if op == '+':
@@ -23,9 +24,11 @@ def calcular(num1, num2):
     elif op == '**':
         return num1 ** num2
 
+''' Imprimindo resultado obtido '''
 res = calcular(num1, num2)
 print('O resultado é: ', res)
 
+''' Looping de repetição do código ate o usuário dizer que não deseja continuar '''
 while cont :
     
     continuar = str(input('Deseja continuar? (s/n) '))
